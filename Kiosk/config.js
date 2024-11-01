65adb15dd98b906a7d884f3846c6a7c28438604e
Michael Shearer <michael@gtpcorp.com>
2024-11-01 15:33:39 -0400

    Remove config.js

--- Kiosk/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "optionA": "Pick up trash around town",
-    "optionB": "Say \"hello\" to my neighbor",
-    "optionC": "Hold the door for someone",
-    "reportingUrl": false,
-    "placeIdentifier": "Location123"
-}
