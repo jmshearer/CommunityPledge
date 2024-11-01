4cbced145f9de134d230179e2decbef6cdeee211
Michael Shearer <michael@gtpcorp.com>
2024-11-01 16:13:37 -0400

    Add node.js receiver

---
+++ Server/Node.js/README.md
@@ -0,0 +1,22 @@
+# Node.js Receiver
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/) (version 12 or higher)
+- [npm](https://www.npmjs.com/) (Node Package Manager, typically installed with Node.js)
+
+## Installation
+
+1. Install dependencies
+
+```
+npm install
+```
+
+2. Run the server
+
+```
+node server.js
+```
+
+The server will start on `http://localhost:3000`.  Update `config.js` accordingly.
---
+++ Server/Node.js/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// JSONP endpoint to receive data
+app.get('/report', (req, res) => {
+    const callback = req.query.callback; // JSONP callback parameter
+    const option = req.query.option;
+    const placeIdentifier = req.query.placeIdentifier;
+    const timestamp = req.query.timestamp;
+
+    // Log the received data (or store it in a database)
+    console.log('Data received:', { option, placeIdentifier, timestamp });
+
+    // Response data
+    const response = {
+        message: "Data received",
+        option: option,
+        placeIdentifier: placeIdentifier,
+        timestamp: timestamp
+    };
+
+    // Send JSONP response
+    if (callback) {
+        res.type('application/javascript');
+        res.send(`${callback}(${JSON.stringify(response)})`);
+    } else {
+        res.status(400).send('Callback parameter is required for JSONP.');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
